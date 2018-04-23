fac171b1523e746c1ed2947e08c848857e7ba05a
knorjador <headers@protonmail.com>
2018-04-23 10:33:50 +0100

    Add icons

--- assets/js/explorer.js
+++ assets/js/explorer.js
@@ -1,33 +1,42 @@
 
-/**** **** **** **** **** **** **** ****
-  > POST
-**** **** **** **** **** **** **** ****/
-
-const post = (data, cb) => { //console.log(data)
-
-  const xhr = new XMLHttpRequest()
-
-  xhr.open('post', 'php/ajax.php')
-
-  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-
-  xhr.send(`data=${JSON.stringify(data)}`)
-
-  xhr.onreadystatechange = () => { //console.log(xhr)
-
-    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
-      cb(data.exercise, xhr.responseText)
-
-  }
-
-}
-
-/**** **** **** **** **** **** **** ****
-  > INIT
-**** **** **** **** **** **** **** ****/
-
-const init = function() {
-
-
-
-}()
+// /**** **** **** **** **** **** **** ****
+//   > POST
+// **** **** **** **** **** **** **** ****/
+//
+// const post = (data, cb) => { //console.log(data)
+//
+//   const xhr = new XMLHttpRequest()
+//
+//   xhr.open('post', 'php/ajax.php')
+//
+//   xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+//
+//   xhr.send(`data=${JSON.stringify(data)}`)
+//
+//   xhr.onreadystatechange = () => { //console.log(xhr)
+//
+//     if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
+//       cb(data.exercise, xhr.responseText)
+//
+//   }
+//
+// }
+//
+// /**** **** **** **** **** **** **** ****
+//   > CALLBACKS
+// **** **** **** **** **** **** **** ****/
+//
+// const render = (data => { //console.log(data)
+//
+// }
+//
+// /**** **** **** **** **** **** **** ****
+//   > INIT
+// **** **** **** **** **** **** **** ****/
+//
+// const init = function() {
+//
+//   post({ display: 'folders'}, render)
+//   // eventNavigation()
+//
+// }()
--- explorer.php
+++ explorer.php
@@ -1,25 +1,56 @@
 
 <?php
 
-// phpinfo()
-
+// root folder
 $folder = 'root/';
 
 // contain folders & files in $folder less . current folder & .. parent folder
 $scaffolding = array_diff(scandir($folder), array('.', '..'));
 
-// var_dump($scaffolding);
+// var_dump($scaffolding); die();
 
-foreach ($scaffolding as $element) {
+// declare empty arrays
+$folders  = [];
+$files    = [];
 
-  $step = $folder.'/'.$element;
+// open list
+$data = '<ul>';
 
-  if(is_dir($step))
-    echo '<p>Dossier : '.$element.'</p>';
-  else
-    echo '<p>Fichier : '.$element.'</p>';
+// loop to check if element is folder or file and push in good array
+foreach ($scaffolding as $element)
+  is_dir($folder.'/'.$element) ? array_push($folders, $element) : array_push($files, $element);
 
-}
+// loop on folders
+foreach($folders as $folder)
+  $data .= '<li class="folder"><img class="i-folder" src="assets/icons/folder.png" alt="folder icon">'.ucfirst($folder).'</li>';
 
+// loop on files
+foreach($files as $file)
+  $data .= '<li class="file"><img class="i-file" src="assets/icons/file.png" alt="file icon">'.ucfirst($file).'</li>';
 
-?>
+// close list
+$data .= '</ul>';
+
+?><DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href=".png"/>
+    <link rel="stylesheet" href="assets/css/lib/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/explorer.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+
+      <h1>Explorator</h1>
+
+      <div class="content">
+        <?= $data; ?>
+      </div>
+
+    </div>
+    <script src="assets/js/explorer.js"></script>
+  </body>
+</html>
