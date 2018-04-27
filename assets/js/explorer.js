3ee1640e25b18f008b5da587decbe443f3e8d35c
knorjador <headers@protonmail.com>
2018-04-27 09:32:03 +0100

    Add view

--- assets/js/explorer.js
+++ assets/js/explorer.js
@@ -1,1 +1,17 @@
 
+const   views   = document.querySelectorAll('.b-view')
+      , length  = views.length;
+
+for(let i = 0; i < length; i++) {
+
+  const view = views[i];
+
+  view.addEventListener('click', e => {
+
+    e.preventDefault();
+
+    window.location.href = `${view.dataset.uri}${view.dataset.view}`;
+
+  });
+
+}
--- index.php
+++ index.php
@@ -2,171 +2,47 @@
 <?php
 
 require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/php/functions.php';
 
 define('ROOT_FOLDER', 'root');
 
-$loader = new Twig_Loader_Filesystem('./templates');
-$twig   = new Twig_Environment($loader, array(
+$twig = new Twig_Environment(new Twig_Loader_Filesystem('./templates'), [
 
-    // 'cache' => './cache'
     'cache' => false
   , 'debug' => true
 
-));
+]);
 
-function checkGet(): array {
+if(empty($_GET)) {
 
-  if(empty($_GET)) {
+  echo $twig->render('simple.html', getHome());
 
-    return [
+} else {
 
-        'breadcrumb'    => getBreadcrumb(ROOT_FOLDER)
-      , 'scaffolding'   => listFolder(ROOT_FOLDER, false)
+  if(isset($_GET['view'])) {
 
-    ];
+    $view = $_GET['view'];
 
-  } else {
+    switch($view) {
 
-    if(isset($_GET['path']) && isset($_GET['sound'])) {
+      case 'simple':
+        $template = 'simple';
+        break;
 
-      $path = $_GET['path'];
+      case 'details':
+        $template = 'details';
+        break;
 
-      if(substr($path, 0, 4) === 'root' && !strpos($path, '..') && file_exists($path)) {
-
-        return [
-
-            'breadcrumb'    => getBreadcrumb($path)
-          , 'scaffolding'   => listFolder($path, $_GET['sound'])
-
-        ];
-
-      } else {
-
-        return [
-
-            'error'       => 'Désolé, ce dossier n\'est pas accessible.'
-          , 'breadcrumb'  => getBreadcrumb(ROOT_FOLDER)
-
-        ];
-
-      }
-
-    } else {
-
-      return [
-
-          'error'       => 'Désolé, une erreur s\'est produite.'
-        , 'breadcrumb'  => getBreadcrumb(ROOT_FOLDER)
-
-      ];
+      default:
+        $template = 'error';
+        break;
 
     }
 
-  }
-
-}
-
-function getBreadcrumb(string $path): array {
-
-  $exploded   = explode('/', $path);
-  $breadcrumb = [];
-  $step       = '';
-
-  foreach($exploded as $explode) {
-
-    $step .= $explode.'/';
-
-    array_push($breadcrumb, ['name' => $explode, 'path' => substr($step, 0, -1)]);
+    echo $twig->render($template.'.html', getData($view));
 
   }
 
-  return $breadcrumb;
-
 }
-
-function listFolder(string $folder, string $sound): array {
-
-  $scaffolding = array_diff(scandir($folder), array('.', '..'));
-
-  $folders = $files = [];
-
-  foreach($scaffolding as $element) {
-
-    $path = $folder.DIRECTORY_SEPARATOR.$element;
-
-    if(is_dir($path)) {
-
-      array_push($folders, [
-
-          'name'    => $element
-        , 'path'    => $path
-
-      ]);
-
-    } else {
-
-      array_push($files, [
-
-          'icon'    => getIcon($element)
-        , 'name'    => $element
-        , 'path'    => $path
-        , 'type'    => finfo_file(finfo_open(FILEINFO_MIME_TYPE), $path)
-        , 'size'    => filesize($path)
-        , 'owner'   => posix_getpwuid(fileowner($path))['name']
-        , 'chmod'   => decoct(fileperms($path) & 0777)
-        , 'date'    => date('d F Y - H:i:s', filemtime($path))
-
-      ]);
-
-    }
-
-  }
-
-  return [
-
-      'folders'   => $folders
-    , 'files'     => $files
-    , 'sound'     => $sound === 'true' ? getRandomSound() : false
-
-  ];
-
-}
-
-function getIcon(string $file): string {
-
-  $exploded   = explode('.', $file);
-  $extension  = end($exploded);
-
-  if($extension === 'png' || $extension === 'jpeg' || $extension === 'jpg' || $extension === 'gif') {
-
-    $icon = 'image';
-
-  } else if($extension === 'mp3') {
-
-    $icon = 'music';
-
-  } else if($extension === 'mp4') {
-
-    $icon = 'video';
-
-  } else {
-
-    $icon = 'file';
-
-  }
-
-  return $icon;
-
-}
-
-function getRandomSound() {
-
-  $sounds = array_values(array_diff(scandir('assets/sounds'), array('.', '..')));
-
-  return $sounds[array_rand($sounds)];
-
-}
-
-echo $twig->render('explorer.html', checkGet());
 
 ?>
